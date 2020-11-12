8692ee5729b6782409a0912028256864db73b230
demosfen10 <ilya.eliseev.1999@mail.ru>
2020-11-12 11:34:18 +0300

    LogIn, should add transition

--- src/components/Auth_form.js
+++ src/components/Auth_form.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import '../Auth_form.css';
 
 class Auth_form extends Component
 {
@@ -6,17 +7,24 @@ class Auth_form extends Component
     render() {
         return (
             <div className ="container">
-                <label for ="firstName">FirstName</label>
+            {/*  <div className ='LogIn_form'> */}
+                <div classname = 'Welcome'>
+                    Welcome
+                </div>
+                <div classname = 'text_to_login'>
+                    Login to get started!
+                </div>
+                <label for ="email">Email:</label>
+                <input
+                type = "email"
+                name ="email"
+                id="email"/>
+                <label for ="password">Password:</label>
                 <input
                 type = "text"
-                name ="firstName"
-                id="firstName"/>
-                <label for ="lastName">LastName</label>
-                <input
-                type = "text"
-                name ="lastName"
-                id="lastName"/>
-                <button>Auth_form</button>
+                name ="password"
+                id="password"/>
+                <button>LogIn</button>
             </div>
         )
     }
